39c581bca116ff95e21b90d943c50360a2913e53
Sruthi Ranganathan <67775776+Sruthi-19@users.noreply.github.com>
2023-06-22 20:52:55 +0530

    Delete Serverless: Breaking a large serverless.yml into manageable chunks/serverless-medium directory

--- Serverless: Breaking a large serverless.yml into manageable chunks/serverless-medium/functions/uuidGenerator/uuidGenerator.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const uuid = require('uuid');
-module.exports.handler = async event => {
-  const generatedId = uuid.v4();
-  return {
-    statusCode: 200,
-    body: generatedId
-  };
-};
